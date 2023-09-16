83e55d578eb0581694fc75831bda1aaf873b593b
KimWang Lee <kimwang6957@gmail.com>
2023-09-16 18:13:28 +0800

    checking error

--- law_project/settings.py
+++ law_project/settings.py
@@ -26,11 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "django-insecure-02&_%-io6y*!%ye*j@3go_*nn=-k(6%vvr5jdaj@36jk#!qudm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
